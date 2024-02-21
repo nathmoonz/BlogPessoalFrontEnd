e557ec8e946a02cd2c2c386eb558846d299f09e1
Teste teste <nathmoonz@hotmail.com>
2024-02-21 13:44:36 -0300

    telas e lógica da tela de editar, criar e excluir tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './assets/components/footer/Footer'
-import Navbar from './assets/components/navbar/Navbar'
-import Home from './paginas/home/Home'
-import Login from './paginas/login/Login'
-import Cadastro from './paginas/cadastro/Cadastro'
-import { AuthProvider } from './contexts/AuthContext'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./assets/components/footer/Footer";
+import Navbar from "./assets/components/navbar/Navbar";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
+import Cadastro from "./paginas/cadastro/Cadastro";
+import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./assets/components/temas/listaTemas/ListaTemas";
+import FormularioTema from "./assets/components/temas/formularioTema/FormularioTema";
+import DeletarTema from "./assets/components/temas/deletarTema/DeletarTema";
 
 function App() {
-
   return (
     <>
-    <AuthProvider>
-      <BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
           <Navbar />
-          <div className='min-h-[80vh]'>
+          <div className="min-h-[80vh]">
             <Routes>
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/assets/components/navbar/Navbar.tsx
+++ src/assets/components/navbar/Navbar.tsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
 
 function Navbar() {
+  let navigate = useNavigate();
+
+  const { usuario, handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert("Usuário deslogado!");
+    navigate("/login");
+  }
+
+  let navComponent;
+
   return (
     <>
       <div className="w-full bg-indigo-900 text-white flex justify-center py-4">
@@ -8,13 +22,23 @@ function Navbar() {
           <div className="text-2xl font-bold uppercase">Blog Pessoal</div>
 
           <div className="flex gap-4">
-            <Link to='/login' className="hover:underline">Login</Link>
-            <Link to='/home' className="hover:underline">Home</Link>
-                <div className="hover:underline">Postagens</div>
-                <div className="hover:underline">Temas</div>
-                <div className="hover:underline">Cadastrar Tema</div>
-                <div className="hover:underline">Perfil</div>
-                <div className="hover:underline">Sair</div>
+            <Link to="/login" className="hover:underline">
+              Login
+            </Link>
+            <Link to="/home" className="hover:underline">
+              Home
+            </Link>
+            <div className="hover:underline">Postagens</div>
+            <Link to="/temas" className="hover:underline">
+              Temas
+            </Link>
+            <Link to="/cadastroTema" className="hover:underline">
+              Cadastrar Tema
+            </Link>
+            <div className="hover:underline">Perfil</div>
+            <Link to="" onClick={logout} className="hover:underline">
+              Sair
+            </Link>
           </div>
         </div>
       </div>
